a3726eb4e34ab05f045ddad2ed29d36fbeddc066
Shamorr <shamorrwilliams@Shamorrs-MacBook-Pro.local>
2023-07-25 20:33:19 -0400

    code for block 13

---
+++ index.js
@@ -0,0 +1,51 @@
+let myString = "I am a string"
+if (myString === "I am a string") {
+console.log(true)
+}
+
+
+else {
+    console.log(false)   
+}
+
+
+let boolean = false
+if(boolean === true){
+    console.log(true)
+
+}
+else {console.log("The boolean value flase is falsy")}
+
+let notAnum = null 
+if(notAnum > 0){console.log(true)}
+else{console.log("The null value is falsy")}
+
+let zero = undefined
+if(zero === 3){console.log(true)}
+else{console.log("undefined is falsy")}
+
+let myNum = 0
+if(myNum > 10){
+    console.log(no)
+}
+else{console.log("The number 0 is falsy (the only falsy number)")}
+
+let myStringTwo = ""
+if(myStringTwo === "hello there"){
+    console.log(correct)
+}
+else{console.log("The empty string is falsy(the only falsy string)")}
+//
+
+
+
+
+let num1 = 50
+let num2 = 51
+if(num1 + num2 === 101){
+    console.log("101 is greater than 100")}
+else if(num1 + num2 <= 100 ){
+    console.log("less than")
+}
+else{console.log("try again")}
+
